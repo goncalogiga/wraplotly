3c238131752c0c64148ff8b654dd9763d35f95f1
goncalogiga <principal.maiagiga@gmail.com>
2023-01-26 21:49:05 +0100

    Added colored lines (usefull for time series analysis)

--- wraplotly/charts.py
+++ wraplotly/charts.py
@@ -46,3 +46,42 @@ class line(draw):
             return go.Scatter(x=self.df[self.x], y=self.df[self.y], **self.kwargs)
         elif self._wraplotly_context == "go":
             return go.Scatter(x=self.x, y=self.y, **self.kwargs)
+
+
+class colored_line(draw):
+    def __init__(self, df=None, x=None, y=None, color=None, **kwargs):
+        self.kwargs = kwargs
+        self.df = df
+        self.x = x
+        self.y = y
+        self.color = color
+
+    def __plot_fn__(self):
+        if self.df:
+            x, y, color = self.df[self.x], self.df[self.y], self.df[self.color]
+        else:
+            x, y, color = self.x, self.y, self.color
+
+        figures, colors_in_legend = [], set()
+
+        for tn in range(len(x)):
+            if color[tn] not in colors_in_legend:
+                name = str(color[tn])
+                showlegend = True
+                colors_in_legend.add(color[tn])
+            else:
+                showlegend = False
+                name = None
+            
+            figures.append(
+                go.Scatter(
+                    x=x[tn : tn + 2],
+                    y=y[tn : tn + 2],
+                    line_color=px.colors.qualitative.Plotly[color[tn]],
+                    name=name,
+                    showlegend=showlegend,
+                    **self.kwargs
+                )
+            )
+
+        return go.Figure(figures)
